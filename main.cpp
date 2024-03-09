3b506bd1ce50ad624685d987ab40c4a184688030
plantew <plantew@union.edu>
2024-03-09 21:09:01 +0000

    include interface in main

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include "mbed.h"
+#include "arm_book_lib.h"
+#include "interface.h"
 
 // main() runs in its own thread in the OS
 int main()
 {
-    void userInterfaceInit();
+
+    userInterfaceInit();
 
     while (true) {
 
-        void userInterfaceUpdate();
+        userInterfaceUpdate();
     }
 }
 
